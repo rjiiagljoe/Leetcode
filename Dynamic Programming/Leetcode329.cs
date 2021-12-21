7417b931f20b4af36a43e2eca185009be39a693e
rjiiagl <rjiiagl@gmail.com>
2021-12-21 18:57:29 +0800

    Add Leetcode 62

--- Dynamic Programming/Leetcode329.cs
+++ Dynamic Programming/Leetcode329.cs
@@ -6,14 +6,30 @@ using System.Threading.Tasks;
 
 
 //Add Leetcode 329 2021.12.20
+/*
+Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+ 
+From each cell, you can either move in four directions: left, right, up, or down. 
+You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
 
-namespace Leetcode
+Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+Output: 4
+Explanation: The longest increasing path is [1, 2, 6, 9].
+
+Input: matrix = [[3,4,5],[3,2,6],[2,2,1]]
+Output: 4
+Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+
+Input: matrix = [[1]]
+Output: 1
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 200
+0 <= matrix[i][j] <= 2^31 - 1
+ */
+namespace Leetcode.Dynamic_Programming
 {
-
-    class DynamicProgramming
-    {
-
-    }
 
     class Leetcode329
     {
@@ -27,31 +43,33 @@ namespace Leetcode
             {
                 for (int j = 0; j < matrix[i].Length; j++)
                 {
-                    longest = Math.Max(longest, LongestIncreasingPathOne(matrix, i, j));
+                    longest = Math.Max(longest, DFS(matrix, i, j));
                 }
             }
             return longest;
         }
 
-        private int LongestIncreasingPathOne(int[][] matrix, int i, int j)
+        private int DFS(int[][] matrix, int i, int j)
         {
             if (_cache[i] != null && _cache[i][j] > 0) return _cache[i][j];
 
             var longest = 0;
             if (i - 1 >= 0 && matrix[i][j] < matrix[i - 1][j])
-                longest = Math.Max(longest, LongestIncreasingPathOne(matrix, i - 1, j));
+                longest = Math.Max(longest, DFS(matrix, i - 1, j));
             if (i + 1 < matrix.Length && matrix[i][j] < matrix[i + 1][j])
-                longest = Math.Max(longest, LongestIncreasingPathOne(matrix, i + 1, j));
+                longest = Math.Max(longest, DFS(matrix, i + 1, j));
             if (j - 1 >= 0 && matrix[i][j] < matrix[i][j - 1])
-                longest = Math.Max(longest, LongestIncreasingPathOne(matrix, i, j - 1));
+                longest = Math.Max(longest, DFS(matrix, i, j - 1));
             if (j + 1 < matrix[i].Length && matrix[i][j] < matrix[i][j + 1])
-                longest = Math.Max(longest, LongestIncreasingPathOne(matrix, i, j + 1));
+                longest = Math.Max(longest, DFS(matrix, i, j + 1));
 
             longest++;
             if (_cache[i] == null) _cache[i] = new int[matrix[i].Length];
             _cache[i][j] = longest;
             return longest;
         }
+
+
     }
 
     
---
+++ Dynamic Programming/Leetcode62.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Input: m = 3, n = 7
+Output: 28
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+
+Input: m = 7, n = 3
+Output: 28
+
+Input: m = 3, n = 3
+Output: 6
+
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 10^9.
+ */
+
+//Add 2021.12.21
+
+namespace Leetcode.Dynamic_Programming
+{
+    class Leetcode62
+    {
+
+        int[,] visited;
+        int[,] memo;
+
+        public int UniquePaths(int m, int n)
+        {
+            visited = new int[m, n];
+            memo = new int[m, n];
+            return dfs(m, n, 0, 0);
+        }
+
+        public int dfs(int m, int n, int i, int j)
+        {
+            if (i == m - 1 && j == n - 1) return 1;
+            int sum = 0;
+            // Let (i, j) be visited for current dfs recursion state
+            visited[i, j] = 1;
+            // Console.WriteLine(i + ", " + j);
+
+            if (i + 1 < m && j < n && visited[i + 1, j] != 1)
+            {
+                sum += memo[i + 1, j] != 0 ? memo[i + 1, j] : dfs(m, n, i + 1, j);
+            }
+            if (i < m && j + 1 < n && visited[i, j + 1] != 1)
+            {
+                sum += memo[i, j + 1] != 0 ? memo[i, j + 1] : dfs(m, n, i, j + 1);
+            }
+
+            // Set (i, j) back to un-visited for former dfs recursion state
+            visited[i, j] = 0;
+            return memo[i, j] = sum;
+        }
+
+      
+    }
+}
